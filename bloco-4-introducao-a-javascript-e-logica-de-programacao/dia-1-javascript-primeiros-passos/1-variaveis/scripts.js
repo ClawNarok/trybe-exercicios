0aa0db4da215c084ce14d55403d14fcf4fa04ded
ClawNarok <arthurvgf@gmail.com>
2021-11-29 16:05:01 -0300

    Exercicio para fixar 1

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/1-variaveis/scripts.js
@@ -0,0 +1,14 @@
+const myName = "Arthur";
+const birthCity = "Santos";
+let birthYear = 1994;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+//birthCity = "Plutão";
+//Erro ocorreu pq a váriavel do tipo const não pode ter seu valor alterado, pois ela contem um valot constante
+console.log(birthCity);
