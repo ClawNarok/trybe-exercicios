a7b58ac7509bf0032ef3ff03c44a41d15ea453a5
ClawNarok <arthurvgf@gmail.com>
2021-11-30 18:03:45 -0300

    condicionando com OR

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4-operadores-logicos/fixar-2-or/scripts.js
@@ -0,0 +1,10 @@
+let weekDay = "quarta-feira";
+
+if (weekDay === "Segunda-feira" || weekDay === "Terça-feira" || weekDay === "Quarta-feira" || weekDay === "Quinta-feira" || weekDay === "Sexta-feira")
+{
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else
+{
+    console.log("FINALMENTE, descanso merecido UwU");
+}
