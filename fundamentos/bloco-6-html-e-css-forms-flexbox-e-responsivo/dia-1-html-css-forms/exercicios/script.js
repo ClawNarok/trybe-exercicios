091b80c3fd223d1e33a64552c4c2fefed07277dd
ClawNarok <arthurvgf@gmail.com>
2022-01-11 11:28:08 -0300

    Exercicios até o começo do 2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -0,0 +1,43 @@
+const estados = [['AC',	'Acre'],
+['AL',	'Alagoas'],
+['AP',	'Amapá'],
+['AM',	'Amazonas'],
+['BA',	'Bahia'],
+['CE',	'Ceara'],
+['DF',	'Distrito Federal'],
+['ES',	'Espírito Santo'],
+['GO',	'Goiás'],
+['MA',	'Maranhão'],
+['MT',	'Mato Grosso'],
+['MS',	'Mato Grosso do Sul'],
+['MG',	'Minas Gerais'],
+['PA',	'Pará'],
+['PB',	'Paraíba'],
+['PR',	'Paraná'],
+['PE',	'Pernambuco'],
+['PI',	'Piauí'],
+['RJ',	'Rio de Janeiro'],
+['RN',	'Rio Grande do Norte'],
+['RS',	'Rio Grande do Sul'],
+['RO',	'Rondônia'],
+['RR',	'Roraima'],
+['SC',	'Santa Catarina'],
+['SP',	'São Paulo'],
+['SE',	'Sergipe'],
+['TO',	'Tocantins']];
+
+const select = document.querySelector('select');
+
+
+function criaOption(value, txt,  pai )
+{
+    let item = document.createElement('option');
+    item.value = value.toLowerCase();
+    item.innerText = txt;
+    pai.appendChild(item);
+}
+
+for (let x = 0; x < estados.length; x++)
+{
+    criaOption(estados[x][0], estados[x][1], select);
+}
